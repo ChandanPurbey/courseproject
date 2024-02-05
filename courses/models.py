4ab6b6392ac6709d8d3745f8a2a8eda98592f90f
Chandan Purbey <66569043+ChandanPurbey@users.noreply.github.com>
2024-02-05 10:31:49 +0530

    first commit

---
+++ courses/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+# Create your models here.
+class Course(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    instructor = models.CharField(max_length=50)
+    
+    class Meta:
+        db_table = 'course_data'
+    
+    def __str__(self):
+        return self.title
+        
